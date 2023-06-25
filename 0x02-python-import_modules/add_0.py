92de7cc19364055fe2b17493729e58041ea187c7
Adem Mohammed <45444428+abulhuman@users.noreply.github.com>
2023-06-25 14:20:54 +0000

    0. Import a simple function from a simple file

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This is the "add" module.
+"""
+
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
