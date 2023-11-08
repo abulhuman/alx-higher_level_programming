45be9f704121049b183fb661b961fbcae87cf410
Adem Mohammed <abulhuman.dev@gmail.com>
2023-11-08 10:27:46 -0800

    [solution]: 0x01 #13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,73 +2,108 @@
 #include <stdlib.h>
 #include "lists.h"
 
+int find_node(listint_t **h, int number);
+listint_t *insert_nodeint_at_index(listint_t **head, int idx, int n);
+
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * insert_node - inserts a new node at
+ * a given position
+ * @head: a pointer to a pointer to a listint_t struct
+ * @number: value of the n member of the new node
+ *
+ * Return: a pointer to the new node or NULL if it
+ * fails
+ *
  */
-size_t print_listint(const listint_t *h)
+
+listint_t *insert_node(listint_t **head, int number)
 {
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
+  listint_t **ptr = head;
+  listint_t *insert = NULL;
+  int idx;
 
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
-
-	return (n);
+  idx = find_node(ptr, number);
+  insert = insert_nodeint_at_index(ptr, idx, number);
+  return (insert);
 }
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
+ * insert_nodeint_at_index - inserts a new node at
+ * a given position
+ * @head: a pointer to a pointer to a listint_t struct
+ * @idx: the index where the node should be added
+ * @n: value of the n element of the node
+ *
+ * Return: a pointer to the new node or NULL if it
+ * fails
+ *
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+
+listint_t *insert_nodeint_at_index(listint_t **head, int idx, int n)
 {
-	listint_t *new;
-	listint_t *current;
+  listint_t **current = head;
+  listint_t *access = *head;
+  listint_t *newnode;
+  listint_t *prev = NULL;
+  int i = 0;
 
-	current = *head;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-	else
+  if (head == NULL)
+    return (NULL);
+  newnode = malloc(sizeof(listint_t));
+  if (newnode == NULL)
+    return (NULL);
+  newnode->n = n;
+  if (idx == 0)
+    {
+      newnode->next = *current;
+      *current = newnode;
+    }
+  else
+    {
+      while (i != idx)
 	{
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new;
+	  if (access->next != NULL)
+	    {
+	      prev = access;
+	      access = access->next;
+	      ++i;
+	    }
+	  else if (idx <= (i + 1))
+	    {
+	      access->next = newnode;
+	      return (newnode);
+	    }
+	  else
+	    return (NULL);
 	}
-
-	return (new);
+      newnode->next = access;
+      prev->next = newnode;
+    }
+  return (newnode);
 }
 
 /**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
+ * find_node - returns number of the first node
+ * with an int greater than the int being inserted
+ * @h: pointer to the first element of the list
+ * @number:int to be inserted
+ *
+ * Return: index of first node greater than number
+ *
  */
-void free_listint(listint_t *head)
-{
-	listint_t *current;
 
-	while (head != NULL)
+int find_node(listint_t **h, int number)
+{
+  listint_t *numnode = *h;
+  int count = 0;
+
+  while (numnode != NULL)
+    {
+      if (numnode->n < number)
 	{
-		current = head;
-		head = head->next;
-		free(current);
+	  count++;
 	}
+      numnode = numnode->next;
+    }
+  return (count);
 }
